667aa5f4694ed45dcfced745cab0b4d50dced140
dimitrisniras <niras.94@gmail.com>
2017-02-21 13:42:21 +0200

    Nothing

--- app/src/main/java/com/healthtrackerinc/healthtracker/activity_phramacy_menu.java
+++ app/src/main/java/com/healthtrackerinc/healthtracker/activity_phramacy_menu.java
@@ -1,7 +1,10 @@
 package com.healthtrackerinc.healthtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_phramacy_menu extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class activity_phramacy_menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phramacy_menu);
+
+        Button drugBtn = (Button)findViewById(R.id.drugBtn);
+
+        // Register the onClick listener
+        drugBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(activity_phramacy_menu.this, DrugActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        Button customersBtn = (Button)findViewById(R.id.customersbtn);
+
+        // Register the onClick listener
+        customersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(activity_phramacy_menu.this, CustomersActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
